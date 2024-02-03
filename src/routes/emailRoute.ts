db3f748275f96433c3a58afeb3def17c82308ea8
Gean Victor <geanvictorduque@gmail.com>
2024-02-02 23:12:10 -0300

    refactor: Refactor EmailRoute

--- src/routes/emailRoute.ts
+++ src/routes/emailRoute.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
 import EmailController from '../controllers/EmailController';
+import { EmailService } from '../services/EmailService'
 
 const router = express.Router();
+const emailController = new EmailController(new EmailService());
 
-router.post('sendEmail', EmailController.sendEmail);
+router.post('sendEmail', emailController.sendEmail);
 
 export default router;
