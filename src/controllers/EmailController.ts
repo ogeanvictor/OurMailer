ab884d37484f7e1fefc59b90c4f26b126e410a2a
Gean Victor <geanvictorduque@gmail.com>
2024-02-02 23:02:03 -0300

    feat: Add EmailController

---
+++ src/controllers/EmailController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+
+import { EmailService } from '../services/EmailService';
+
+class EmailController {
+    private emailService:EmailService;
+
+    constructor(emailService:EmailService) {
+        this.emailService = emailService;
+    }
+
+    async sendEmail(req:Request, res:Response):Promise<void> {
+        try {
+            const email = await EmailService.sendEmail(req.body);
+            res.status(StatusCodes.OK).send(email);
+        } catch (error) {
+            res.status(StatusCodes.BAD_REQUEST).send(error);
+        }
+    }
+}
+
+export default EmailController;
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Import routes
-import emailRoutes from './routes/emailRoute';
+import emailRoutes from './routes/EmailRoute';
 
 app.use(cors());
 
