057dc159cba6cf1088219256c0a59cbc60963f57
Gean Victor <geanvictorduque@gmail.com>
2024-02-26 15:40:28 -0300

    feat: Create controller uploadFile

--- backend/src/controllers/EmailController.ts
+++ backend/src/controllers/EmailController.ts
@@ -18,6 +18,15 @@ class EmailController {
             res.status(StatusCodes.BAD_REQUEST).send(error);
         }
     }
+
+    async uploadFile(req:Request, res:Response):Promise<void> {
+        try {
+            const file = await this.emailService.uploadFile(req.file);
+            res.status(StatusCodes.OK).send(file);
+        } catch (error) {
+            res.status(StatusCodes.BAD_REQUEST).send(error);
+        }
+    }
 }
 
 export default EmailController;
