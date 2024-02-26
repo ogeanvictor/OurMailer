8578a528f8e6c1f5a97709547eb0b353f685f086
Gean Victor <geanvictorduque@gmail.com>
2024-02-26 16:08:35 -0300

    feat: Add route uploadFile

--- backend/src/routes/EmailRoute.ts
+++ backend/src/routes/EmailRoute.ts
@@ -8,7 +8,7 @@ const router = express.Router();
 const upload = multer();
 const emailController = new EmailController(new EmailService());
 
-router.post('/uploadFile', upload.fields([{ name: "document"}]), emailController.uploadFile)
+router.post('/uploadFile', upload.fields([{ name: "document"}]), emailController.uploadFile);
 router.post('/sendEmail', emailController.sendEmail.bind(emailController));
 
 export default router;
